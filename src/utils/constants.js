b15048a465016274f2cedc3f48b342ce32422e21
mnatasyar <mnatasyar0@gmail.com>
2025-06-22 20:25:13 +0700

    change base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  "https://2619-2a09-bac5-3a5a-1028-00-19c-282.ngrok-free.app";
+  "https://6029-2a09-bac5-3a5a-1028-00-19c-282.ngrok-free.app";
 export const OUTPUT_IMAGE_URL = `${BASE_URL}/output`;
 export const OUTPUT_VIDEO_URL = `${BASE_URL}/output_video`;
