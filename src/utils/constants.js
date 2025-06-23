d0324931dcc84827c6c1c1a062b0ab71824fc45e
M Natasya Ramadana <93390884+mnatasyar@users.noreply.github.com>
2025-06-23 07:03:59 +0700

    change base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://api.mnatasyar.com";
+export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "https://api.mnatasyar.com";
 export const OUTPUT_IMAGE_URL = `${BASE_URL}/output`;
 export const OUTPUT_VIDEO_URL = `${BASE_URL}/output_video`;
