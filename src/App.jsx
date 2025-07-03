49e7ad5d61d3beb657d6abd7f6c96ab24796de35
M. Natasya R <m.natasyar@Ms-MacBook-Air.local>
2025-07-03 14:43:47 +0700

    ganti desain tampilan

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ import React from "react";
 function AnalyzePage() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-xl font-bold mb-4 text-center">
-        {"Deteksi Wajah dan Pengenalan Ekspresi Siswa SD menggunakan Model YOLOv8 dan ResNet50"}
-        <br />
-        {"untuk Mengukur Ketertarikan terhadap Mata Pelajaran"}
-      </h1>
       <FileUploadForm />
     </div>
   );
